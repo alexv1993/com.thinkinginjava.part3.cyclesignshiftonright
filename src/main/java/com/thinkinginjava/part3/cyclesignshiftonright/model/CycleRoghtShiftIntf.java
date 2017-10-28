370dc8b2608eccc64e1dde7abaacea2d19e6d7ce
alexv1993 <alexvendin@gmail.com>
2017-10-28 20:40:30 +0300

    a base interface for declaring a name of main functionality for this task :)

---
+++ src/main/java/com/thinkinginjava/part3/cyclesignshiftonright/model/CycleRoghtShiftIntf.java
@@ -0,0 +1,8 @@
+package com.thinkinginjava.part3.cyclesignshiftonright.model;
+
+/**
+ * Created by Alex on 28.10.2017.
+ */
+public interface CycleRoghtShiftIntf {
+    public void doShift(int num);
+}
